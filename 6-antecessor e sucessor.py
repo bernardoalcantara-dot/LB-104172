161e0dba31434ef9700f0867a3b2c3db973e2933
bernardoalcantara-dot <bernardo.alcantara@ba.estudante.senai.br>
2026-08-21 16:52:12 -0300

    Add files via upload

---
+++ 6-antecessor e sucessor.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls")
+
+numero = int(input('digite o numero'))
+sucessor = numero + 1
+antecessor = numero -1
+print('o numero antecessor', antecessor)
+print('o numero sucessor', sucessor)
